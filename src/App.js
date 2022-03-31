ccb6081be44b5aafd14ad0979ba9d7f3d4e9db13
arunkudiyal <arunkudiyal@icloud.com>
2022-03-31 12:32:48 +0530

    React Props & Destructring of Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello... Welcome to React!</h1>
+      <Home></Home> <br />
+      <Button />
+      <Button color="danger" text="Submit" />
+      <Button color="warning" />
+      <Button text="Click Here" />
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'
+
+const Home = () => {
+    return(
+        <div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit ex nisi aliquid inventore deserunt et ullam expedita quas voluptatem veritatis quidem autem, officia, harum, modi cum at cupiditate atque. Cupiditate nemo, aut amet commodi expedita ducimus perferendis soluta voluptates beatae!</p>
+
+            <img id="home-img" src="https://2.img-dpreview.com/files/p/TS600x450~sample_galleries/3141499184/6105939280.jpg" alt="" />
+        </div>
+    )
+}
+
+export default Home;
