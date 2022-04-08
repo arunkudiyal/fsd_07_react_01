6179ce3cd01ea2d80a84e4c9924680d5a65aea08
arunkudiyal <arunkudiyal@icloud.com>
2022-04-08 12:27:48 +0530

    States in Function-based Components, Passing Paremeters to Event Functions, Inline Styling in JSX

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
     someOtherState: 'Some Other State Value'
   }
 
-  onClickEventListner = () => {
+  onClickEventListner = (e) => {
+    console.log(e)
     // DON NOT DO THIS : this.state.persons[0].name = 'Steph'
     this.setState({
       persons: [
@@ -26,26 +27,84 @@ class App extends Component {
     })
   }
 
-  render() {
-    console.log(this.state)
+  switchNameHandler = (e) => {
+    this.setState({
+      persons: [
+        { name: e.target.value, age: 30 },
+        { name: "Elena", age: 29 },
+        { name: "Jacky", age: 25 }
+      ]
+    })
+  }
 
+  render() {
     return (
       <div className='container'>
         <h1 className='page-header'>Person Management System</h1>
         <Person 
           name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} />
+          age={this.state.persons[0].age}
+          changed={(e) => this.switchNameHandler(e)} />
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age} />
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age}> This is Person - 2 </Person>
-
-        <button onClick={this.onClickEventListner} className='btn btn-danger'>Switch Data</button>
+        
+        <br /> <br />
+        <button onClick={(e) => this.onClickEventListner(e)} className='btn btn-danger'>Switch Data</button>
       </div>
     );
   }
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+
+// import Person from './components/Person/Person';
+
+// import './App.css';
+
+// const App = () => {
+//     const [personState, setPersonState] = useState({
+//       persons: [
+//         { name: "Stephan", age: 30 },
+//         { name: "Elenea", age: 27 },
+//         { name: "Jack", age: 25 }
+//       ]
+//     });
+
+//     const [someOtherState, setSomeOtherState] = useState('Some Other State Value')
+
+//     const onClickEventListner = () => {
+//       // DON NOT DO THIS : this.state.persons[0].name = 'Steph'
+//       setPersonState({
+//         persons: [
+//           { name: "Steph", age: 30 },
+//           { name: "Elena", age: 29 },
+//           { name: "Jacky", age: 25 }
+//         ]
+//       })
+//     }
+
+//     return(
+//       <div className='container'>
+//          <h1 className='page-header'>Person Management System</h1>
+//          <Person 
+//            name={personState.persons[0].name} 
+//            age={personState.persons[0].age} />
+//          <Person 
+//            name={personState.persons[1].name} 
+//            age={personState.persons[1].age} />
+//          <Person 
+//            name={personState.persons[2].name} 
+//            age={personState.persons[2].age}> This is Person - 2 </Person>
+
+//          <button onClick={onClickEventListner} className='btn btn-danger'>Switch Data</button>
+//        </div>
+//     )
+// }
+
+// export default App;
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,21 @@
 const Person = (props) => {
+    const textBoxStyle = {
+        border: '2px solid red',
+        padding: '5px'
+    }
+
     // Props -> A way to access the data from outside the component
     return(
         <div>
             {/* <p className="lead">{props.children}</p> */}
             <p>My name is {props.name} and I am {props.age} years old </p>
+
+            {/* INLINE STYLE IN JSX */}
+            {/* <input style={ {border: '2px solid navy'} } onChange={props.changed} type="text" placeholder="enter new name..." /> */}
+
+            {/* PASSING STYLES AS OBJECTS IN JSX */}
+            <input style={ textBoxStyle } onChange={props.changed} type="text" placeholder="enter new name..." />
+            <hr />
         </div>
     )
 }
